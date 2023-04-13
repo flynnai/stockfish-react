044fea1b462874b38c61e0ea190fd045d3b4aa95
flyai <flynnai@umich.edu>
2023-04-13 10:36:27 +0200

    Stockfish loaded, chesspiece images added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+const engine = new Worker(process.env.PUBLIC_URL + "/stockfish.js");
 
 function App() {
-  return (
-    <div>asdf</div>
-  );
+    const [output, setOutput] = useState("");
+    const hasRunRef = useRef(false);
+
+    const blockForMessage = () =>
+        new Promise((resolve) => {
+            const handleMessage = (message) => {
+                engine.removeEventListener("message", handleMessage);
+                resolve(message);
+            };
+            engine.addEventListener("message", handleMessage);
+        });
+
+    useEffect(() => {
+        if (hasRunRef.current) return;
+        hasRunRef.current = true;
+
+        const runAsync = async () => {
+            let event;
+
+            engine.postMessage("uci");
+            event = await blockForMessage();
+            engine.postMessage("uci");
+
+            console.log( " > " + event.data);
+
+            // setOutput(output => output + "\n" + event.data);
+            engine.postMessage("ucinewgame");
+            engine.postMessage("setoption name MultiPV value 5");
+            engine.postMessage("position startpos");
+            engine.postMessage("go depth 15");
+        };
+
+        runAsync();
+    }, []);
+
+    return <div>{output}</div>;
 }
 
 export default App;
